6b7d55b5e06b73819837c4ea48294aa2b6bfac47
Noor Clean <noorclean98@gmail.com>
2021-02-02 22:06:56 +0700

    add FETCH API function and event listener

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 const search = document.getElementById('search');
 const matchList = document.getElementById('match-list');
+
+//SEARCH STATES.JSON AND FILTER IT WITH FETCH API
+const searchStates = async searchText => {
+    const res = await fetch('../data/state.json');
+    const states = await res.json();
+
+}
+
+search.addEventListener('input', () => searchStates(search.value));
