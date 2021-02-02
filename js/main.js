d484670714429288938da32274c01f9511955c7e
Noor Clean <noorclean98@gmail.com>
2021-02-02 22:12:02 +0700

    add REGEX function for text input

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@ const searchStates = async searchText => {
     const res = await fetch('../data/state.json');
     const states = await res.json();
 
+    //GET MATCHES TO CURRENT TEXT INPUT
+    let matches = states.filter(state => {
+        const regex = new RegExp(`^${searchText}`, 'gi');
+        return state.name.match(regex) || state.abbr.match(regex);
+    });
+
 }
 
 search.addEventListener('input', () => searchStates(search.value));
